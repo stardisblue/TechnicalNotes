042971b26db89e5981f22093f47f294d7ae037a5
stardisblue <stardisblue@gmail.com>
2016-03-08 18:06:37 +0100

    adds Entity and begin database construction

---
+++ app/entity/TechnoteEntity.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * TechnicalNotes <https://www.github.com/stardisblue/TechnicalNotes>
+ * Copyright (C) 2016  TechnicalNotes Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace techweb\app\entity;
+
+use techweb\core\Entity;
+
+class TechnoteEntity extends Entity
+{
+    public function __construct()
+    {
+        $properties = [
+            'id' => null,
+            "titre" => '',
+            "contenu" => '',
+            "date modification" => null,
+            "date creation" => null,
+            "user_id" => null,
+        ];
+
+        parent::__construct($properties);
+    }
+
+}
---
+++ core/Entity.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * TechnicalNotes <https://www.github.com/stardisblue/TechnicalNotes>
+ * Copyright (C) 2016  TechnicalNotes Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: stardisblue
+ * Date: 08/03/16
+ * Time: 16:22
+ */
+
+namespace techweb\core;
+
+use techweb\core\exception\UnknownPropertyException;
+
+abstract class Entity
+{
+    protected $options = [];
+
+    /**
+     * Entity constructor.
+     * @param array $properties ["property" => "default value"]
+     * @param array $options additionnals options
+     */
+    public function __construct(array $properties, array $options = [])
+    {
+        foreach ($properties as $property => $value) {
+            $this->$property = $value;
+        }
+
+        $this->options = $options;
+    }
+
+    /**
+     * Set a property only if it was declared in the constructor
+     *
+     * @param array $properties , ["property" => "default value"]
+     * @throws UnknownPropertyException
+     */
+    public function set(array $properties)
+    {
+        foreach ($properties as $key => $value) {
+            if (isset($this->$key)) {
+                $this->$key = $value;
+            } else {
+                throw new UnknownPropertyException('No attribute ' . $key . ' in this Entity');
+            }
+        }
+    }
+
+    /**
+     * Return the reference of the property
+     *
+     * @param string $properties
+     * @return mixed
+     * @throws UnknownPropertyException
+     */
+    public function &get(string $properties)
+    {
+        if (isset($this->$properties)) {
+            return $this->$properties;
+        } else {
+            throw new UnknownPropertyException("No matching property");
+        }
+    }
+}
