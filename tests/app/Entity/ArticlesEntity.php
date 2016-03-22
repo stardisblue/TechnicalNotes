29ec3554130110dc8fbdd8c57aff96647dbf884e
stardisblue <stardisblue@gmail.com>
2016-03-22 16:31:40 +0100

    Ajout des fichiers test

---
+++ tests/app/Entity/ArticlesEntity.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stardisblue
+ * Date: 22/03/16
+ * Time: 15:35
+ */
+
+namespace techweb\tests\app\Entity;
+
+
+class ArticlesEntity
+{
+
+}
---
+++ tests/app/model/ArticlesModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stardisblue
+ * Date: 22/03/16
+ * Time: 15:32
+ */
+
+namespace app\model;
+
+
+class ArticlesModel
+{
+
+}
