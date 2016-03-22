d6f1985f85ca6f5416d3f4526dc86555343c33a4
garad1 <adam_1995@live.fr>
2016-03-22 17:12:33 +0100

    Ajout Entity anwsers

---
+++ app/entity/AnwsersEntity.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * TechnicalNotes <https://www.github.com/stardisblue/TechnicalNotes>
+ * Copyright (C) 2016  TechnicalNotes Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace techweb\app\entity;
+
+use techweb\core\database\ORM\Entity;
+
+/**
+ * Class TechnoteEntity
+ * @package techweb\app\entity
+ */
+class AnwsersEntity extends Entity
+{
+    /**
+     * TechnoteEntity constructor.
+     */
+    public function __construct()
+    {
+        $columns = [
+            'id' => null,
+            'user_id' => null,
+            'question_id' => null,
+            'content' => '',
+            'creation_date' => '',
+        ];
+
+        $options = ['primary' => 'id', 
+            'belongTo' => [
+                    ['table' => 'users', 'foreing_key' => 'user_id'],
+                    ['table' => 'questions', 'foreing_key' => 'question_id']
+                ],
+            'hasMany' => [
+                ['table' => 'anwser_comments'],
+            ]
+        ];
+
+        parent::__construct($columns, $options);
+    }
+
+}
